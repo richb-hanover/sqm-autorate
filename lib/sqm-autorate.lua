30a412ac12cb5688cc472b422d5808d3b33312a6
Fail-Safe <Fail-Safe@users.noreply.github.com>
2021-12-30 01:27:18 -0500

    Missed one.

--- lib/sqm-autorate.lua
+++ lib/sqm-autorate.lua
@@ -133,7 +133,7 @@ local function conductor()
     local threads = {
         pinger_thread = lanes.gen("*", {
             required = {"bit32", "posix.sys.socket", "posix.time", "vstruct"}
-        }, pinger.ts_ping_sender)(sock, tick_duration, packet_id),
+        }, pinger.ts_ping_sender)(sock, packet_id),
         receiver_thread = lanes.gen("*", {
             required = {"bit32", "posix.sys.socket", "posix.time", "vstruct"}
         }, receiver.ts_ping_receiver)(sock, stats_queue, packet_id),
